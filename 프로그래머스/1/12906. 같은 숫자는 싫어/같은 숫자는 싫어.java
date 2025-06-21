ac9ea788fea2aa8d3b8a5f43ddaa96c1e8b703d5
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-06-21 12:52:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 54.22 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> s = new Stack<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (s.isEmpty() || s.peek() != arr[i])
+                s.push(arr[i]);
+        }
+        int[] answer = new int[s.size()];
+        for (int i = s.size() -1; i >= 0; i--) {
+            answer[i] = s.pop();
+        }
+        return answer;
+    }
+}
