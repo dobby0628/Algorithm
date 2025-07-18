0813d7e275db3d8cfa68ada40967010e3dcf1b44
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-07-18 10:53:43 +0900

    Create 556.java

---
+++ jungol/556.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		
+		for (int i = 0; i < 10; i++) {
+			arr[i] = i + 1;
+		}
+		
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
