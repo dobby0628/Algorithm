28e7a2c8bd95fd6d7fbb9d9e3c51ad528a22de37
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-07-28 23:11:35 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 416 ms, Memory: 37604 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+// 아이디어 : 높이를 스택에 넣어 높이 차이를 통해 건물의 개수 찾기
+//			스택의 peek보다 낮은 수가 나올 때까지 push
+//			낮은 수가 나오면 pop하면서 result++
+//			y = 0일 경우 지금까지 스택에 있던 값 모두 pop하면서 result++
+
+// 1try 틀림 : 높이가 0일 때 고려를 안한 것이 건물의 개수를 잘못 셈
+// 2try 틀림 : 건물을 더하는 시점이 잘못됨
+
+public class Main {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = Integer.parseInt(sc.nextLine());
+	    Stack<Integer> h = new Stack<>();
+	    int result = 0;
+	    
+	    for (int i = 0; i < n; i++) {
+	        String[] tmp = sc.nextLine().split(" ");
+	        int y = Integer.parseInt(tmp[1]);
+
+	        //System.out.println("i:"+i+", y:"+y);
+	        if (y == 0) {
+	        	while (!h.isEmpty()) {
+	        		h.pop();
+	        		result++;
+	        	}
+	        }
+	        else if (h.isEmpty())
+	            h.push(y);
+	        else if (h.peek() < y)
+	        	h.push(y);
+	        else {
+	        	while (!h.isEmpty() && h.peek() > y) {
+	        		h.pop(); result++;
+	        	}
+	        	if (h.isEmpty() || h.peek() < y)
+	        		h.push(y);
+	        }
+	        //System.out.println("s:"+h+", result:"+result);
+	    }
+	    while (!h.isEmpty()) {
+    		h.pop();
+    		result++;
+    	}
+	    System.out.println(result);
+	}
+}
