4c424da57d2b5db0fa769bf4252f4bf5c245e8b8
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-08-07 11:02:04 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,3 +1,6 @@
+// 문자를 입력받으므로 숫자로 변환했을 때 자료형의 범위를 벗어나는지 생각해야함
+// char -> int : - '0' or Charater.getNumericValue()  사용 가능
+
 import java.io.*;
 import java.util.*;
 public class Main {
@@ -16,3 +19,4 @@ public class Main {
 		bw.flush();
 	}
 }
+
