9a82501f1e7c3e34ae7af24b7bc72c641c9a5de3
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-08-07 11:36:58 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,3 +1,7 @@
+// 문제를 잘못 이해해서 오래걸림 -> 문제 제대로 이해하고 풀기
+
+// Double의 결과값이 나와야하므로 정수끼리 계산해서 소수값이 사라지지 않도록 유의해야함
+
 import java.util.Scanner;
 
 public class Main {
@@ -17,4 +21,5 @@ public class Main {
 			sum += (arr[i] * 1.0) / (max * 1.0) * 100.0;
 		System.out.println(sum / N);
 	}
+
 }
