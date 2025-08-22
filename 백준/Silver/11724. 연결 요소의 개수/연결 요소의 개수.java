9206d295f970b2c0f7873b6bcaa53e2e2abd62bd
dobby0628 <54130173+dobby0628@users.noreply.github.com>
2025-08-22 16:10:12 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 141892 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,3 +1,11 @@
+/*
+ * # 문제 이해
+ * 방향 없는 그래프가 주어졌을 때, 연결 요소의 개수를 구하는 프로그램 작성하기
+ * 
+ * # 문제 풀이
+ * dfs로 연결된 요소들의 개수 구하기
+ */
+
 import java.util.*;
 import java.io.*;
 
@@ -5,47 +13,45 @@ public class Main {
 	static List<List<Integer>> graph;
 	static boolean[] visited;
 	
-	public static void dfs(int node) {
+	static void dfs(int node) {
 		visited[node] = true;
-		
-		for (int a : graph.get(node)) {
-			if (!visited[a]) {
-				visited[a] = true;
-				dfs(a);
-			}
+		for (int next : graph.get(node)) {
+			if (!visited[next])
+				dfs(next);
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		visited = new boolean[N +1];
 		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++)
+		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
+		}
+		visited = new boolean[N+1];
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			
 			graph.get(from).add(to);
 			graph.get(to).add(from);
 		}
 		
-		for (int i = 1; i <= N; i++)
-			Collections.sort(graph.get(i));
-		
-		int count = 0;
-		for (int i = 1; i <=N; i++) {
-			if (!visited[i]) {
-				dfs(i);
-				count++;
+		int cnt = 0;
+		for (int node = 1; node <= N; node++) {
+			if (!visited[node]) {
+				cnt++;
+				dfs(node);
 			}
 		}
-		System.out.println(count);
+		System.out.println(cnt);
 	}
 }
